4d70a010514e4fea079e2338e7777e53bebf9fef
jianpeng.ren <jianpeng.ren@vhall.com>
2021-12-01 13:58:29 +0800

    add redisson

--- im-server/src/main/java/com/itrjp/im/server/ServerMain.java
+++ im-server/src/main/java/com/itrjp/im/server/ServerMain.java
@@ -1,6 +1,6 @@
 package com.itrjp.im.server;
 
-import com.itrjp.im.server.server.SocketIOServer;
+import com.itrjp.im.server.server.SocketIoServer;
 
 /**
  * 程序主入口
@@ -9,6 +9,6 @@ import com.itrjp.im.server.server.SocketIOServer;
  */
 public class ServerMain {
     public static void main(String[] args) {
-        SocketIOServer.run(args);
+        SocketIoServer.run(args);
     }
 }
