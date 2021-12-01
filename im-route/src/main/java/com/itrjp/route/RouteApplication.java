3727fb042d7b8cb19a9be4153a1c218e57bb3a9b
renjp <979668507@qq.com>
2021-12-01 22:37:21 +0800

    添加im-route 模块

---
+++ im-route/src/main/java/com/itrjp/route/RouteApplication.java
@@ -0,0 +1,14 @@
+package com.itrjp.route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * im route 服务, 主要用来路由server
+ */
+@SpringBootApplication
+public class RouteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RouteApplication.class, args);
+    }
+}
